9c71de1dca82ed5a29707657f2affb15dc5aaa35
Stephen Nene <stephen.nene@student.moringaschool.com>
2025-01-01 11:04:10 +0300

    add @vercel/analytics for improved tracking and create vercel.json for rewrites

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "./assets/styles/index.css";
-import App from './app/App'
+import { Analytics } from "@vercel/analytics/react";
 
 import { BrowserRouter } from 'react-router-dom';
+
+import "./assets/styles/index.css";
+import App from './app/App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+      <Analytics />
       <App />
     </BrowserRouter>
   </StrictMode>,
